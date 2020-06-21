3363fec4a1f07cef7741b98e21f4c34a12cc4521
João Batista <jbdionisio@hotmail.com.br>
2020-06-21 15:04:14 -0300

    first commit

---
+++ src/main/java/com/dionisio/dionisiofoodapi/DionisioFoodAPI.java
@@ -0,0 +1,13 @@
+package com.dionisio.dionisiofoodapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DionisioFoodAPI {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DionisioFoodAPI.class, args);
+	}
+
+}
