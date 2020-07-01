68664855777fe9df9bb6429d961810f31eb5b3a1
João Batista <jbdionisio@hotmail.com.br>
2020-06-30 21:34:30 -0300

    Mapeando primeiras entidades

---
+++ src/main/java/com/dionisio/dionisiofoodapi/model/Cozinha.java
@@ -0,0 +1,52 @@
+package com.dionisio.dionisiofoodapi.model;
+
+import java.util.Objects;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Cozinha {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return this.nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == this)
+            return true;
+        if (!(o instanceof Cozinha)) {
+            return false;
+        }
+        Cozinha cozinha = (Cozinha) o;
+        return Objects.equals(id, cozinha.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+
+
+}
---
+++ src/main/java/com/dionisio/dionisiofoodapi/model/Restaurante.java
@@ -0,0 +1,65 @@
+package com.dionisio.dionisiofoodapi.model;
+
+import java.math.BigDecimal;
+import java.util.Objects;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Restaurante {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+
+    @Column(name = "taxa_frete")
+    private BigDecimal taxaFrete;
+
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return this.nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public BigDecimal getTaxaFrete() {
+        return this.taxaFrete;
+    }
+
+    public void setTaxaFrete(BigDecimal taxaFrete) {
+        this.taxaFrete = taxaFrete;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == this)
+            return true;
+        if (!(o instanceof Restaurante)) {
+            return false;
+        }
+        Restaurante restaurante = (Restaurante) o;
+        return Objects.equals(id, restaurante.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+
+}
